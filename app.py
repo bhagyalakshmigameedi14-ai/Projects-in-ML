13c530992b9190a5d898550d34e62936dc4a3f82
Your Name <you@example.com>
2026-08-22 21:15:04 +0530

    template is ready

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def predict_datapoint():
     else:
         data = CustomData(
             gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('race_ethnicity'),
+            race_ethnicity=request.form.get('race_ethnicity') or request.form.get('ethnicity'),
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
@@ -33,7 +33,7 @@ def predict_datapoint():
         print(pred_df)
         predict_pipeline = PredictPipeline()
         results = predict_pipeline.predict(pred_df)
-        return render_template('home.html', results=results)
+        return render_template('home.html', results=results[0])
 
 
 if __name__ == "__main__":
